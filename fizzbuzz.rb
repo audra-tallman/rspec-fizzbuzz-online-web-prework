c9562f38bf6453f59a82edfe48ce2c5036f1fb46
Audra Tallman <audra.tallman@hotmail.com>
2019-02-20 21:22:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,13 @@
 
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0 
-    
+  "FizzBuzz"
     num % 3 == 0
     return "Fizz"
   elsif num % 5 == 0
     return "Buzz"
   elsif num % 3 == 0 && num % 5 == 0
-    return "FizzBuzz"
+    return 
   else 
     return "nil"
   end
